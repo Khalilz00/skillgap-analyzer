0c08d9025f8c529eab0097706b0e7025f9d1a644
Khalilz00 <khalil.zanzouri@student.junia.com>
2026-05-03 13:46:33 +0200

    feat(terraform): add BigQuery datasets and IAM roles for dbt service account

--- infra/terraform/bigquery.tf
+++ infra/terraform/bigquery.tf
@@ -1,5 +1,16 @@
-resource "google_bigquery_dataset" "skillgap" {
-  dataset_id                  = "skillgap_${var.environment}"
-  location                    = "EU"
-  default_table_expiration_ms = 7776000000 # 90 jours en millisecondes
+resource "google_bigquery_dataset" "raw" {
+  dataset_id                  = "raw_${var.environment}"
+  location                    = "europe-west1"
+  default_table_expiration_ms = 90 * 24 * 60 * 60 * 1000
+}
+
+resource "google_bigquery_dataset" "staging" {
+  dataset_id                  = "staging_${var.environment}"
+  location                    = "europe-west1"
+  default_table_expiration_ms = 30 * 24 * 60 * 60 * 1000
+}
+
+resource "google_bigquery_dataset" "marts" {
+  dataset_id = "marts_${var.environment}"
+  location   = "europe-west1"
 }
